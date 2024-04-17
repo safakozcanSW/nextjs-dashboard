66e54419cfb56584e459fde219cb9328456749a5
Turgut Şafak Özcan <safakozcansw@gmail.com>
2024-04-18 01:25:30 +0300

    Fatura düzenleme işlemleri tamamlandı

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -15,6 +15,7 @@ const FormSchema = z.object({
   status: z.enum(['pending', 'paid']),
   date: z.string(),
 });
+// !======================================================================
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
@@ -43,4 +44,27 @@ export async function createInvoice(formData: FormData) {
   // Test it out:
   // console.log(rawFormData);
   // console.log(typeof rawFormData.amount);
+}
+
+// !======================================================================
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
